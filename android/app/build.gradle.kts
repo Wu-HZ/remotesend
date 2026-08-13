18cbf86e636ffccb01161c7773991487430d40b5
Wu-HZ <102032485+Wu-HZ@users.noreply.github.com>
2026-08-13 08:44:26 +0800

    fix: 固定NDK版本27，消除F-Droid构建警告

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.remotesend.remote_send"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
